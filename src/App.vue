c5888bf1708eff62a76b92a656d666b3db309ad7
Marcelo Sousa <marcelo.sousa@lbca.com.br>
2019-09-13 15:34:04 -0300

    O poder das diretivas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>{{titulo}}</h1>
-    <img :src="foto.url" :alt="foto.url">
+
+    <ul>
+      <li v-for="foto of fotos"> 
+          <img :src="foto.url" :alt="foto.url">
+      </li>
+    </ul>
   </div> 
 </template>
 
@@ -10,13 +15,20 @@
     data(){
       return {
         titulo: 'Alurapic',
-        foto: {
-          url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
-          titulo: 'Cachorro'
-        }
-      }
+        fotos: [
+          {
+            url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+            titulo: 'Cachorro'
+          },
+          {
+            url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+            titulo: 'Cachorrao'
+          }
+        ]
     }
   }
+}    
+  
 </script>
 
 <style>
