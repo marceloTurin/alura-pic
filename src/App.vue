3e82f37a80550e8f475bce8534932f437054680f
Marcelo Sousa <marcelo.sousa@lbca.com.br>
2019-09-13 19:34:32 -0300

    Filtrando efetivamente a lista

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <h1 class="centralizado">{{titulo}}</h1>
     <input type="serach" class="filtro" v-on:input="filtro = $event.target.value"  placeholder="filtre por parte do titulo">
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
         </meu-painel> 
@@ -29,6 +29,16 @@
         fotos: [],
         filtro:''
           
+    }
+  },
+  computed:{
+    fotosComFiltro(){
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(),'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else{
+        return this.fotos;
+      }
     }
   },
   created(){
