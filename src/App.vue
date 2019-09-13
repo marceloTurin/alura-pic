3a043f51149d38801c964b8826cfc12dc5d8cb3d
Marcelo Sousa <marcelo.sousa@lbca.com.br>
2019-09-13 16:47:51 -0300

    e promises

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,9 @@
     }
   },
   created(){
-    alert('criei o componente');
+    this.$http.get('http://localhost:3000/v1/fotos') // método get retorna uma promise
+      .then(res => res.json()) // retorna a resposta do servidor e  converte em json
+      .then(fotos =>  this.fotos = fotos, err => console.log(err)); //e coloca as informações no nosso array
   }
 }    
   
